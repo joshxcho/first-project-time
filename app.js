0cf36b891eca2132e85ceec77d79de0b84d1b4ae
Josh Cho <joshcho@Joshs-MacBook-Pro.local>
2018-06-05 21:26:34 -0700

    Got it to display all the key value pairs

--- app.js
+++ app.js
@@ -2,41 +2,37 @@ $(document).ready(function(){
   $('.setData').on('click', function(){
    
     let snippetNameValue = $('[name=snippetName]').val();
-    $('.debug').text(snippetNameValue);
+    // $('.debug').text(snippetNameValue);
     let snippetTextValue = $('[name=snippetText]').val();
-    $('.debug').text(snippetTextValue)
+    // $('.debug').text(snippetTextValue)
 
-    localStorage.setItem(snippetNameValue, snippetTextValue);
+    localStorage.setItem(JSON.stringify(snippetNameValue), JSON.stringify(snippetTextValue));
    
-    $('[name=snippetName]').val('');
-    $('[name=snippetText]').val('');
+    // $('[name=snippetName]').val('');
+    // $('[name=snippetText]').val('');
+
+
 
   });
-
+  
   $('.getData').on('click', function(){
-    let retrievedData = localStorage.getItem(snippetNameValue, snippetTextValue);
-    $('.debug').text(retrievedData);
+    // let retrievedData = localStorage.getItem(snippetNameValue, snippetTextValue);
+    // $('.debug').text(retrievedData);
+
+    const snippetArr = [];
+    for (var i = 0; i < localStorage.length; i++) { 
+  
+    snippetArr.push(JSON.parse(localStorage.key(i)) + ' : ' + JSON.parse(localStorage.getItem(localStorage.key(i))))
+      // console.log(snippetArr)
+
+    let $body = $('body');
+    let $texts = $('<span class="text">' +snippetArr+ '</span>');
+    
+    $('.text').remove();
+    $('.container').append($texts);
+
+    }
   });
-
 });
-
-
-  //   let textFieldValue = $('[name=snippetName]').val($('[name=snippetText]').val());
-    
-  //   $('.debug').text(textFieldValue);
-
-
-
-  //   localStorage.setItem('myFormTextData', textFieldValue)
-    
-  //   $('[name=snippetName').val('');
-  //   $('[name=snippetText]').val('')
-   
-  // });
-
-  // $('.getData').on('click', function() {
-  //   let retrievedData = localStorage.getItem('myFormTextData');
-  //   $('.debug').text(retrievedData);
-  // });
 
  
